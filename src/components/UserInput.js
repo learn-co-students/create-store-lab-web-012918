82652a8713e9110cc9bd356e40c5ccdc5cf57219
jaf7 <janthonyfields@gmail.com>
2018-04-23 16:19:14 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      hometown: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      username: e.target.name === 'username' ? e.target.value : this.state.username,
+      hometown: e.target.name === 'hometown' ? e.target.value : this.state.hometown
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch( {type: 'ADD_USER', user: {username: this.state.username, hometown: this.state.hometown} } )
+    this.setState({username: '', hometown: ''})
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit} >
+          <input value={this.state.username} onChange={this.handleChange} name="username" type="text" placeholder="username" />
+          <input value={this.state.hometown} onChange={this.handleChange} name="hometown" type="text" placeholder="hometown" />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
 };
 
-export default UserInput;
+export default UserInput
+
+
+ 
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,13 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {users: []}, action){
+
+  action.user ? console.log(`action in reducer ${action.type} ${action.user.username} ${action.user.hometown}`) : console.log(`action ${action}`)
+
+  switch (action.type) {
+    case 'ADD_USER':
+      return {...state, users: state.users.concat(action.user) }
+
+    default:
+      return state
+  }
 }
+ 
